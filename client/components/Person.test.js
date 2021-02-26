144fe7ba5490d72cc7a8cf193f0a4e28ae735627
Markus Erapolku <markus.erapolku@gmail.com>
2021-02-26 23:05:00 +0200

    Fix lint issues

--- client/components/Person.test.js
+++ client/components/Person.test.js
@@ -2,19 +2,18 @@ import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import { render } from '@testing-library/react';
 
-import Person from './Person'
+import Person from './Person';
 
-describe('Person', () => 
-{
-    test("Person has name and number", () => 
-    {
-        const handleDelete = jest.fn();
-        const person = { name: "kekkonen", number: "salainen" };
-        
-        let component = render(
-          <Person person={person} handleDelete={handleDelete} />);
+describe('Person', () => {
+  test('Person has name and number', () => {
+    const handleDelete = jest.fn();
+    const person = { name: 'kekkonen', number: 'salainen' };
 
-        expect(component.container).toHaveTextContent("kekkonen");
-        expect(component.container).toHaveTextContent("salainen");
-    });
+    const component = render(
+      <Person person={person} handleDelete={handleDelete} />,
+    );
+
+    expect(component.container).toHaveTextContent('kekkonen');
+    expect(component.container).toHaveTextContent('salainen');
+  });
 });
